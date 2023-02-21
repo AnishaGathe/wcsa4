d8f02c27d9d6f6081e487b4645de4bbad9bbc85c
Anisha <Anisha@LAPTOP-ANISHA>
2023-02-21 18:01:44 +0530

    commit2

---
+++ TestActiTimeProject/src/test/java/testPackage/ActiTimeTestValidLogin.java
@@ -0,0 +1,16 @@
+package testPackage;
+
+import org.junit.Test;
+
+public class ActiTimeTestValidLogin {
+	
+	@Test
+	public void M1()
+	{
+		//System .setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+	
+	
+		
+	}
+
+}
